9be762edbb65df46ec4745f2d1a57ab15838e234
Mountains <583082772@qq.com>
2024-10-20 21:56:49 +0800

    添加overlayBuilder

--- media_kit_video/lib/media_kit_video_controls/src/controls/material.dart
+++ media_kit_video/lib/media_kit_video_controls/src/controls/material.dart
@@ -223,6 +223,8 @@ class MaterialVideoControlsThemeData {
   /// Custom builder for seek indicator.
   final Widget Function(BuildContext, double)? speedUpIndicatorBuilder;
 
+  final Widget Function(BuildContext context)? overlayBuilder;
+
   // BUTTON BAR
 
   /// Buttons to be displayed in the primary button bar.
@@ -314,6 +316,7 @@ class MaterialVideoControlsThemeData {
     this.brightnessIndicatorBuilder,
     this.seekIndicatorBuilder,
     this.speedUpIndicatorBuilder,
+    this.overlayBuilder,
     this.primaryButtonBar = const [
       Spacer(flex: 2),
       MaterialSkipPreviousButton(),
@@ -376,6 +379,7 @@ class MaterialVideoControlsThemeData {
     Widget Function(BuildContext, double)? brightnessIndicatorBuilder,
     Widget Function(BuildContext, Duration)? seekIndicatorBuilder,
     Widget Function(BuildContext, double)? speedUpIndicatorBuilder,
+    Widget Function(BuildContext context)? overlayBuilder,
     List<Widget>? primaryButtonBar,
     List<Widget>? topButtonBar,
     EdgeInsets? topButtonBarMargin,
@@ -444,6 +448,7 @@ class MaterialVideoControlsThemeData {
       seekIndicatorBuilder: seekIndicatorBuilder ?? this.seekIndicatorBuilder,
       speedUpIndicatorBuilder:
           speedUpIndicatorBuilder ?? this.speedUpIndicatorBuilder,
+      overlayBuilder: overlayBuilder ?? this.overlayBuilder,
       primaryButtonBar: primaryButtonBar ?? this.primaryButtonBar,
       topButtonBar: topButtonBar ?? this.topButtonBar,
       topButtonBarMargin: topButtonBarMargin ?? this.topButtonBarMargin,
@@ -1151,6 +1156,12 @@ class _MaterialVideoControlsState extends State<_MaterialVideoControls> {
                           }),
                           icon: Icon(lock ? Icons.lock_outlined : Icons.lock_open_outlined),
                         ),
+                      ),
+                    ),
+                    IgnorePointer(
+                      ignoring: !mount,
+                      child: Container(
+                        child: theme(context).overlayBuilder?.call(context),
                       ),
                     ),
                     Positioned.fill(
