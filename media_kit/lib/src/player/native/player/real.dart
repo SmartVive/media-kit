216dbfb7222a050fbd304f1a42d2d2dbebb35341
Mountains <583082772@qq.com>
2024-11-02 00:20:34 +0800

    添加onPreloadedHooks

--- media_kit/lib/src/player/native/player/real.dart
+++ media_kit/lib/src/player/native/player/real.dart
@@ -2491,6 +2491,20 @@ class NativePlayer extends PlatformPlayer {
           prop.ref.id,
         );
       }
+      if (prop.ref.name.cast<Utf8>().toDartString() == 'on_preloaded') {
+        for (final hook in onPreloadedHooks) {
+          try {
+            await hook.call();
+          } catch (exception, stacktrace) {
+            print(exception);
+            print(stacktrace);
+          }
+        }
+        mpv.mpv_hook_continue(
+          ctx,
+          prop.ref.id,
+        );
+      }
     }
   }
 
@@ -2696,11 +2710,15 @@ class NativePlayer extends PlatformPlayer {
       final load = 'on_load'.toNativeUtf8();
       final unload = 'on_unload'.toNativeUtf8();
       final loadFail = 'on_load_fail'.toNativeUtf8();
+      final preloaded = 'on_preloaded'.toNativeUtf8();
       mpv.mpv_hook_add(ctx, 0, load.cast(), 0);
       mpv.mpv_hook_add(ctx, 0, unload.cast(), 0);
       mpv.mpv_hook_add(ctx, 0, loadFail.cast(), 0);
+      mpv.mpv_hook_add(ctx, 0, preloaded.cast(), 0);
       calloc.free(load);
       calloc.free(unload);
+      calloc.free(loadFail);
+      calloc.free(preloaded);
 
       await NativeReferenceHolder.instance.add(ctx);
     });
@@ -2871,6 +2889,8 @@ class NativePlayer extends PlatformPlayer {
   final List<Future<void> Function()> onUnloadHooks = [];
 
   final List<Future<void> Function()> onLoadFailHooks = [];
+
+  final List<Future<void> Function()> onPreloadedHooks = [];
 
   /// Synchronization & mutual exclusion between methods of this class.
   static final Lock lock = Lock();
